74e9e42ff19d540ad26abfeb1e985e1b5c10815c
Jaeder <jaeder42@gmail.com>
2020-02-09 13:07:55 +0100

    stash

--- src/views/profile.jsx
+++ src/views/profile.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 import axios from "axios";
 export const Profile = () => {
-  const [profile, setProfile] = useState(null);
   let { id } = useParams();
+  const [profile, setProfile] = useState(null);
 
   const fetchProfile = async () => {
     const result = await axios.get(
@@ -19,5 +20,56 @@ export const Profile = () => {
     fetchProfile();
   }, []);
 
-  return <div>{profile && <p>{JSON.stringify(profile)}</p>}</div>;
+  return (
+    <div>
+      {profile && (
+        <Wrapper>
+          <Id>{profile.id}</Id>
+          <Title>{profile.comp_rank}</Title>
+          <Title>Commendations</Title>
+          <Commendations>
+            <Row>
+              <Column>Friendly</Column>
+              <Column>Teacher</Column>
+              <Column>Leader</Column>
+            </Row>
+            <Row>
+              <Column>{profile.friendly}</Column>
+              <Column>{profile.teaching}</Column>
+              <Column>{profile.leader}</Column>
+            </Row>
+          </Commendations>
+          <p>{JSON.stringify(profile)}</p>
+        </Wrapper>
+      )}
+    </div>
+  );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Id = styled.h1``;
+
+const Title = styled.h2``;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  align-items: center;
+`;
+
+const Commendations = styled.div`
+  max-width: 500px;
+  min-width: 200px;
+`;
